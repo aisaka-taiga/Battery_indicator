6bd729cfd15a2b8b1b57d40dee82eb20269e2b1f
TAIGA <qwerty0204@gmail.com>
2016-06-28 23:49:00 +0900

    First Release

---
+++ TAIGA_SOFTWARE_BATTERY_INDICATOR/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace BATTERY_INDICATOR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.ddToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(232, 157);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(279, 125);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Text = "배터리 상태";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ddToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.ShowCheckMargin = true;
+            this.contextMenuStrip1.ShowImageMargin = false;
+            this.contextMenuStrip1.Size = new System.Drawing.Size(285, 86);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // ddToolStripMenuItem
+            // 
+            this.ddToolStripMenuItem.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.ddToolStripMenuItem.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ddToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Transparent;
+            this.ddToolStripMenuItem.Name = "ddToolStripMenuItem";
+            this.ddToolStripMenuItem.Overflow = System.Windows.Forms.ToolStripItemOverflow.Always;
+            this.ddToolStripMenuItem.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.ddToolStripMenuItem.ShowShortcutKeys = false;
+            this.ddToolStripMenuItem.Size = new System.Drawing.Size(284, 38);
+            this.ddToolStripMenuItem.Text = "부팅 시 자동실행";
+            this.ddToolStripMenuItem.Click += new System.EventHandler(this.ddToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(876, 584);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ddToolStripMenuItem;
+    }
+}
+
---
+++ TAIGA_SOFTWARE_BATTERY_INDICATOR/Form1.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BATTERY_INDICATOR
+{
+    public partial class Form1 : Form
+    {
+        RegistryKey registryKey = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+        public Form1()
+        {
+            InitializeComponent();
+            string ddd = NextValue().ToString("R");
+            CreateTextIcon(ddd);
+            this.Visible = false;
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+
+
+            System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+            timer.Interval = 30000;
+            timer.Tick += new EventHandler(timer_tick);
+            timer.Start();
+
+            if (registryKey.GetValue("TAIGASOFTWARE_BATTERYSTATUS") == null)
+            {
+                ddToolStripMenuItem.Checked = false;
+            }
+            else
+            {
+                ddToolStripMenuItem.Checked = true;
+                ddToolStripMenuItem.CheckState = CheckState.Checked;
+            }
+        }
+        int Textsize_modi = 10;
+        void timer_tick(object sender, System.EventArgs e)
+        {
+            string ddd = NextValue().ToString("R");
+            CreateTextIcon(ddd);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // MessageBox.Show(ddd);
+        }
+        float NextValue()
+        {
+            System.Windows.Forms.PowerStatus ps = System.Windows.Forms.SystemInformation.PowerStatus;
+            return ps.BatteryLifePercent * 100;
+        }
+        public void CreateTextIcon(string str)
+        {
+            //105
+            Font fontToUse = new Font("Microsoft Sans Serif", Textsize_modi, FontStyle.Regular, GraphicsUnit.Pixel);
+            Brush brushToUse = new SolidBrush(Color.White);
+            if (NextValue() <= 9)
+            {
+                fontToUse = new Font("Microsoft Sans Serif", 88);
+                brushToUse = new SolidBrush(Color.Red);
+            }
+            if (NextValue() >= 10)
+            {//85
+                fontToUse = new Font("Microsoft Sans Serif", 85);
+                brushToUse = new SolidBrush(Color.White);
+            }
+            if (NextValue() == 100)
+            {
+                fontToUse = new Font("Microsoft Sans Serif", 57);
+                brushToUse = new SolidBrush(Color.Green);
+                // MessageBox.Show("배터리가 완전히 충전되었습니다.");
+            }
+            Bitmap bitmapText = new Bitmap(128, 128);
+            Graphics g = System.Drawing.Graphics.FromImage(bitmapText);
+
+            IntPtr hIcon;
+            g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SingleBitPerPixelGridFit;
+            g.DrawString(str, fontToUse, brushToUse, -10, 15);
+            hIcon = (bitmapText.GetHicon());
+            notifyIcon1.Icon = System.Drawing.Icon.FromHandle(hIcon);
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void ddToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (registryKey.GetValue("TAIGASOFTWARE_BATTERYSTATUS") == null)
+            {
+                ddToolStripMenuItem.Checked = true;
+                registryKey.SetValue("TAIGASOFTWARE_BATTERYSTATUS", Application.ExecutablePath.ToString());
+            }
+            if (registryKey.GetValue("TAIGASOFTWARE_BATTERYSTATUS") == null)
+            {
+                registryKey.DeleteValue("TAIGASOFTWARE_BATTERYSTATUS", false);
+            }
+        }
+    }
+}
